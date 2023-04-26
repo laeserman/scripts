13592be709f1eeba7d141467aa09a4816445b407
Lauren Eserman <laeserman@users.noreply.github.com>
2023-04-26 16:56:25 -0400

    Create batchHybpiper.pl

---
+++ batchHybpiper.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+use strict;
+#Lauren Eserman 2019
+#Modified from a script from Karolina Heyduk
+
+#run in directory where reads are located
+
+my $list = $ARGV[0]; #read to ID index file
+my %read1;
+my %read2;
+my @libIDs;
+
+open IN, "<$list";
+while (<IN>) {
+    chomp;
+    my ($libID, $readID) = split /\t/;
+    if ($readID =~ 'R1') {
+        $read1{$libID} = $readID;
+        print "$libID\t$readID\n";
+        if ($libID ~~ @libIDs) {
+            next;
+        }
+        else {
+            push(@libIDs, $libID);
+        }
+    }
+    elsif ($readID =~ 'R2') {
+        $read2{$libID} = $readID;
+        print "$libID\t$readID\n";
+    }
+}
+close IN;
+
+for my $libID (@libIDs) {
+    open OUT, ">$libID.hybpiper.sh";
+    print OUT "python reads_first.py -b mag_kim.fasta -r $libID\_paired\_R1.fastq $libID\_paired\_R2.fastq --prefix $libID --bwa";
+    close OUT;
+    system "bash $libID.hybpiper.sh";
+    system "python cleanup.py $libID";
+}
