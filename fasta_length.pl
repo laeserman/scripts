c130504c3d2fd279e1ed276db2703087cc95b5a5
Lauren Eserman <laeserman@users.noreply.github.com>
2023-11-03 11:42:33 -0400

    Add files via upload

---
+++ fasta_length.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Bio::SeqIO;
+
+# Check for the correct number of command-line arguments
+if (@ARGV != 1) {
+    die "Usage: $0 <input_fasta_file>\n";
+}
+
+my $input_file = $ARGV[0];
+
+# Create a Bio::SeqIO object to read the input FASTA file
+my $seqio = Bio::SeqIO->new(
+    -file   => $input_file,
+    -format => 'fasta'
+    );
+
+# Process each sequence in the input file
+while (my $seq = $seqio->next_seq) {
+    my $seq_name = $seq->display_id;
+    my $seq_length = $seq->length;
+
+    print "Sequence: $seq_name, Length: $seq_length\n";
+}
---
+++ longest_seq.pl
@@ -0,0 +1,37 @@
+#This perl script written by ChatGPT using the prompt "Write a perl script to load in a fasta file, extract the longest DNA sequence, and write the output to a new file. Use the bioperl module"
+
+use strict;
+use Bio::SeqIO;
+
+# Input and output file names
+my $input_file = $ARGV[0];
+my $output_file = $ARGV[1];
+
+# Create a Bio::SeqIO object for reading the input FASTA file
+my $seqio = Bio::SeqIO->new(-file => $input_file, -format => 'fasta');
+
+# Initialize variables to track the longest sequence
+my $longest_seq = undef;
+my $max_length = 0;
+
+# Iterate through the sequences in the input file
+while (my $seq = $seqio->next_seq) {
+    my $sequence = $seq->seq;
+    my $length = length($sequence);
+
+    if ($length > $max_length) {
+	$max_length = $length;
+	$longest_seq = $seq;
+    }
+}
+
+if ($longest_seq) {
+    # Create a Bio::SeqIO object for writing the output FASTA file
+    my $outseq = Bio::SeqIO->new(-file => ">$output_file", -format => 'fasta');
+
+    # Write the longest sequence to the output file
+    $outseq->write_seq($longest_seq);
+    print "Longest sequence written to $output_file\n";
+} else {
+    print "No sequences found in the input file\n";
+}
