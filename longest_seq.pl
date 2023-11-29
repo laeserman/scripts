ff383e53a9dd13d9be0c096e499ff40c9bf3f365
Lauren Eserman <laeserman@users.noreply.github.com>
2023-11-29 10:21:28 -0500

    Update longest_seq.pl

--- longest_seq.pl
+++ longest_seq.pl
@@ -1,37 +1,53 @@
-#This perl script written by ChatGPT using the prompt "Write a perl script to load in a fasta file, extract the longest DNA sequence, and write the output to a new file. Use the bioperl module" with subsequent manual edits by Lauren Eserman-Campbell
+#This perl script written by ChatGPT using the prompt "Write a perl script using the bioperl module to input multiple fasta files all with the .fasta ending, select the longest sequence from each file, and output the longest sequences to a new file". The script was subsequently edited manually by Lauren Eserman-Campbell
+#Usage: perl find_longest_sequences.pl output.fasta *.fasta (*.fasta being all your input gene files)
+
+#!/usr/bin/perl
 
 use strict;
+use warnings;
 use Bio::SeqIO;
 
-# Input and output file names
-my $input_file = $ARGV[0];
-my $output_file = $ARGV[1];
+# Check for the correct number of command-line arguments
+if (@ARGV < 2) {
+    die "Usage: $0 <output_file> <input_files>\n";
+}
 
-# Create a Bio::SeqIO object for reading the input FASTA file
-my $seqio = Bio::SeqIO->new(-file => $input_file, -format => 'fasta');
+# Get the output file name (the first argument)
+my $output_file = shift @ARGV;
 
-# Initialize variables to track the longest sequence
-my $longest_seq = undef;
-my $max_length = 0;
+# Create a Bio::SeqIO object for writing the longest sequences to the output file
+my $outseq = Bio::SeqIO->new(
+    -file   => ">$output_file",
+    -format => 'fasta'
+);
 
-# Iterate through the sequences in the input file
-while (my $seq = $seqio->next_seq) {
-    my $sequence = $seq->seq;
-    my $length = length($sequence);
+# Process each input file
+foreach my $input_file (@ARGV) {
+    my $inseq = Bio::SeqIO->new(
+        -file   => $input_file,
+        -format => 'fasta'
+    );
 
-    if ($length > $max_length) {
-	$max_length = $length;
-	$longest_seq = $seq;
+    my $max_length = 0;
+    my $longest_seq;
+
+    # Iterate through sequences in the input file
+    while (my $seq = $inseq->next_seq) {
+        my $seq_length = $seq->length;
+
+        if ($seq_length > $max_length) {
+            $max_length = $seq_length;
+            $longest_seq = $seq;
+        }
+    }
+
+    if ($longest_seq) {
+        # Write the longest sequence to the output file
+        $outseq->write_seq($longest_seq);
+        print "Longest sequence from $input_file added to $output_file\n";
+    } else {
+        print "No sequences found in $input_file\n";
     }
 }
 
-if ($longest_seq) {
-    # Create a Bio::SeqIO object for writing the output FASTA file
-    my $outseq = Bio::SeqIO->new(-file => ">$output_file", -format => 'fasta');
-
-    # Write the longest sequence to the output file
-    $outseq->write_seq($longest_seq);
-    print "Longest sequence written to $output_file\n";
-} else {
-    print "No sequences found in the input file\n";
-}
+print "Processing complete.\n";
