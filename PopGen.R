ee7a1161e5d676fe722a1cee9689d7e9420766a1
Lauren Eserman <laeserman@users.noreply.github.com>
2023-11-29 13:47:17 -0500

    Update PopGen.R

--- PopGen.R
+++ PopGen.R
@@ -1,21 +1,16 @@
 # Libraries ---------------------------------------------------------------
-library(pegas)
-library(adegenet)
 library(vcfR)
-library(ape)
+library(adegenet)
 library(hierfstat)
-library(dplyr)
 
 # Read Data ---------------------------------------------------------------
 setwd("C:/Users/leserman/Documents/Rhodo_chapmanii/Subsample")
 data <- read.vcfR("Rchap.passfilter.recode.vcf")
 #data2 <- read.vcfR("Rchap.cohort.g.vcf") #Sites before filtering
 
-
 # Convert to both genlight and genind -------------------------------------
 x <- vcfR2genlight(data)
 rchap <- vcfR2genind(data)
-
 
 # Assign populations to individuals ---------------------------------------
 #Pop assignments with 1=Hosford, 2=Gulf
@@ -26,7 +21,6 @@ pop(x) <- as.factor(c("1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1"
 #pop(x) <- as.factor(c("1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "2", "2", "2", "2", "2", "2", "2", "3", "3", "5", "4", "1", "1", "4", "5", "4", "4", "4", "4", "4", "5", "4", "5", "5", "4", "5", "5", "5", "5", "4", "4", "4", "4", "5", "4", "5", "4", "4", "4", "4", "5", "5", "5", "5", "5", "5", "5", "5", "5", "4", "4", "4", "5", "5", "5", "1", "4", "5", "5", "5"))
 #pop(rchap) <- as.factor(c("1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "2", "2", "2", "2", "2", "2", "2", "3", "3", "5", "4", "1", "1", "4", "5", "4", "4", "4", "4", "4", "5", "4", "5", "5", "4", "5", "5", "5", "5", "4", "4", "4", "4", "5", "4", "5", "4", "4", "4", "4", "5", "5", "5", "5", "5", "5", "5", "5", "5", "4", "4", "4", "5", "5", "5", "1", "4", "5", "5", "5"))
 
-
 # Calculate Fst, Ho, He, Fis ----------------------------------------------
 fst <- basic.stats(rchap)
 write.table(fst$Ho, file="Rchap_Ho.txt")
@@ -34,15 +28,12 @@ write.table(fst$Hs, file="Rchap_Hs.txt")
 write.table(fst$Fis, file="Rchap_Fis.txt")
 write.table(fst$overall, file="Rchap_all.txt")
 
-
 # Inbreeding --------------------------------------------------------------
 #Calculate Inbreeding
 temp <- inbreeding(rchap)
 Fbar <- sapply(temp, mean)
 hist(Fbar, col="firebrick", main="Average inbreeding in R chapmanii")
 which(Fbar>0.6)
-
-
 
 # PCA ---------------------------------------------------------------------
 pca1 <- glPca(x, nf=3)
@@ -58,7 +49,6 @@ add.scatter.eig(pca1$eig,2,1,2)
 #col <- funky(9)
 #s.class(pca1$scores, pop(x), xax=1, yax=2, col=transp(col,.6), axesell=FALSE, cstar=0, cpoint=3, grid=FALSE)
 #add.scatter.eig(pca1$eig,2,1,2)
-
 
 # DAPC --------------------------------------------------------------------
 grp <- find.clusters(rchap, max.n.clust=10)
